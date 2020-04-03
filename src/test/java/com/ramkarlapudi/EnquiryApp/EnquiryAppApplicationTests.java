292bccfdd0432ed3e2bddaffb5020e7548d4cf6d
Ramkarlapudi <Ramkarlapudi1995@gmail.com>
2020-04-03 19:09:19 +0530

    initial Commit by ramKarlapudi

---
+++ src/test/java/com/ramkarlapudi/EnquiryApp/EnquiryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramkarlapudi.EnquiryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnquiryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
